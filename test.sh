bd7482d95dfd6254ff21cfb28ab7fc743451ee61
dgz01 <59541437+dgz01@users.noreply.github.com>
2023-01-26 19:19:57 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
 for (i = 0; i < 10; ++i); do
-    (curl -O https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso)
-done
+    (wget --no-check-certificate "https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso" -O target)
