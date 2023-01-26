b81cbcfcf8fb3f8562f0139e5e1ea9003d9f31dc
dgz01 <59541437+dgz01@users.noreply.github.com>
2023-01-26 21:10:00 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 # while true ;do wget --no-check-certificate "https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso" -O target; done;
-for ((i=1; i<=100; i++)); do wget --no-check-certificate https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso -O target; done;
+for ((i=1; i<=100; i++)); do wget --no-check-certificate https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso -O target echo $i; done;
