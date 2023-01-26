35a86a18890094df23dfeb57123d8f09ddb953f7
dgz01 <59541437+dgz01@users.noreply.github.com>
2023-01-26 19:38:06 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-   for(i = 0; i < 10; ++i))
+while true
 do
     wget --no-check-certificate "https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso" -O target
 done
