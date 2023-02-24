e3456f28d98a5d25e16bb8fa2b0c55f999c72e10
dgz01 <59541437+dgz01@users.noreply.github.com>
2023-02-24 09:42:40 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 # for ((i=1; i<=1000; i++)); do echo $i; wget --no-check-certificate https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso -O target && rm -f target && rm -rf /var/log; done; reboot;
 # 1.37
 # while true ; do wget --no-check-certificate "https://releases.ubuntu.com/22.04/ubuntu-22.04.1-live-server-amd64.iso" -O target && rm -f target && rm -rf /var/log; done; reboot;
-for ((i=1; i<=1000; i++)); do echo $i; wget --no-check-certificate https://releases.ubuntu.com/22.04/ubuntu-22.04.1-live-server-amd64.iso -O target && rm -f target && rm -rf /var/log; done; reboot;
+for ((i=1; i<=1000; i++)); do echo $i; wget --no-check-certificate https://gsl-syd.mm.fcix.net/ubuntu-releases/22.10/ubuntu-22.10-desktop-amd64.iso -O target && rm -f target && rm -rf /var/log; done; reboot;
